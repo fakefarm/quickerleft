6fba8b38d0e03efac345f5298f0c299eeeededb4
Dave Woodall <dave.woodall@phishme.com>
2018-04-27 18:25:49 -0400

    spike on pexels

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ ruby '2.5.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0.rc2'
+gem 'httparty'
+gem "bulma-rails", "~> 0.6.2"
+
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
---
+++ app/controllers/albums_controller.rb
@@ -0,0 +1,68 @@
+class AlbumsController < ApplicationController
+  before_action :set_album, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @albums = Album.all
+  end
+
+  def show
+    # 1. service obj that responds to call
+    # 2. @photos = AlbumsService.call(params)
+    # 3. Whenever gem (for cron) or bg job
+    headers = {
+      Authorization: ENV['PEXELS']
+    }
+    response = HTTParty.get( "https://api.pexels.com/v1/search?query=#{@album.name}&per_page=15&page=#{params[:page]}", headers: headers )
+    @photos = response.parsed_response['photos']
+  end
+
+  def new
+    @album = Album.new
+  end
+
+  def edit
+  end
+
+  def create
+    @album = Album.new(album_params)
+
+    respond_to do |format|
+      if @album.save
+        format.html { redirect_to @album, notice: 'Album was successfully created.' }
+        format.json { render :show, status: :created, location: @album }
+      else
+        format.html { render :new }
+        format.json { render json: @album.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @album.update(album_params)
+        format.html { redirect_to @album, notice: 'Album was successfully updated.' }
+        format.json { render :show, status: :ok, location: @album }
+      else
+        format.html { render :edit }
+        format.json { render json: @album.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @album.destroy
+    respond_to do |format|
+      format.html { redirect_to albums_url, notice: 'Album was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_album
+      @album = Album.find(params[:id])
+    end
+
+    def album_params
+      params.require(:album).permit(:name)
+    end
+end
